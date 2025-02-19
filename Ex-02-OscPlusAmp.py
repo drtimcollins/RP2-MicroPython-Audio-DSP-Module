ff7fd087355c120091c4d6ed125e0ab7cbc57431
Tim Collins <47898052+drtimcollins@users.noreply.github.com>
2025-02-19 20:23:29 +0000

    Create Ex-02-OscPlusAmp.py

---
+++ Ex-02-OscPlusAmp.py
@@ -0,0 +1,46 @@
+# Sine wave generator demonstration
+# Copyright (c) 2025 Tim Collins - MIT License
+# See https://github.com/drtimcollins/RP2-MicroPython-Synth-Modules/blob/main/LICENSE
+# Requires I2S device with:
+# GP18 -> Bit Clock
+# GP19 -> LR Clock
+# GP20 -> Data
+
+import time, struct, uctypes
+from machine import I2S, Pin, mem32
+from micropython import const
+from sineOsc import sineOsc         # Requires sineOsc.py and sineTable.dat to be in RP2 filespace
+from DCA import DCA
+
+led = Pin('LED')                    # LED is used as simple indicator of processor load.
+
+Fs = 24000                              # Sample rate = 24 kHz
+BUFFER_LEN = 2000                       # Shorter buffer = less latency but higher processing overhead
+
+sck,ws,sd = Pin(18), Pin(19), Pin(20)   # Also known as bit clock, LR clock, and data
+audio_out = I2S(0, sck=sck, ws=ws, sd=sd, mode=I2S.TX,
+                bits=16, format=I2S.MONO, rate=Fs, ibuf=BUFFER_LEN)
+
+print('I2S enabled')
+
+sineBuf = bytearray(BUFFER_LEN)
+gainBuf = bytearray(BUFFER_LEN)
+outBuf = bytearray(BUFFER_LEN)
+sOsc = sineOsc(sineBuf)                     # Create sine wave generator
+dca = DCA(sineBuf, gainBuf, outBuf)
+
+for n in range(BUFFER_LEN/2):
+    struct.pack_into('<H',gainBuf,n*2,300)
+
+try:
+    while True:                         # Infinite loop - press Ctrl-C to exit
+        led.on()                        
+        sOsc.processBuffer(0x0555)      # Fills buf with samples, 0x555 = ~500 Hz
+        dca.processBuffer()
+        led.off()
+        audio_out.write(outBuf)
+except (KeyboardInterrupt, Exception) as e:
+    print(f"Caught exception: {type(e).__name__}")
+
+audio_out.deinit()                      # Must deinit or pico will need resetting
+print('I2S disabled')
